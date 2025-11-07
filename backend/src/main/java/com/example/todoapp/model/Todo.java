6edcc0db862d84d3f13e9571506bdfb7b77b11f9
jacob6838 <51723791+jacob6838@users.noreply.github.com>
2025-11-07 13:57:07 -0700

    Swash Merge Test
    * Removing package-lock
    
    * Adding spring-boot-starter-security
    
    * Create TodoRepositoryCopy.java
    
    * Removing spaces in todo app

--- backend/src/main/java/com/example/todoapp/model/Todo.java
+++ backend/src/main/java/com/example/todoapp/model/Todo.java
@@ -27,15 +27,12 @@ public class Todo {
     public Long getId() {
         return id;
     }
-    
     public void setId(Long id) {
         this.id = id;
     }
-    
     public String getTitle() {
         return title;
     }
-    
     public void setTitle(String title) {
         this.title = title;
     }
