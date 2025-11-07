57cf625a7852562e0b6d6c66586584f71a1aedad
jacob6838 <jfrye@neaeraconsulting.com>
2025-11-07 13:54:51 -0700

    Tweaking spacing in Todo.java

--- backend/src/main/java/com/example/todoapp/model/Todo.java
+++ backend/src/main/java/com/example/todoapp/model/Todo.java
@@ -15,7 +15,6 @@ public class Todo {
     
     @Column(nullable = false)
     private boolean completed;
-    
     public Todo() {
     }
     
@@ -27,6 +26,7 @@ public class Todo {
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
